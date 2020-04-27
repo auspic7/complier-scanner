68723749178bedc98689898d6ffa73fde1eb6642
Myeongwon Choi <auspic7@gmail.com>
2020-04-27 22:02:47 +0900

    test input

--- input.c
+++ input.c
@@ -2,7 +2,7 @@ int func (char c, bool b, float f){
     int a = 0 + 1200000003 + -1;
     a = 0-0;
     char __c1 = "hello world this is an example 123 ";
-    bool b_1234  =  true false;
+    bool b_1234  = true false;
     float f_  = 0.000012301 + -1233.3 + 33000000003+ 0.0;
 
     if(b_1234 != false) {
@@ -11,5 +11,5 @@ int func (char c, bool b, float f){
         return 1 << 3;
     }
 }
-
-0 1 22 123 56 001 &$
+Ture___r
+0 1 22 123 56 001 "asdfagwet"
