3ccf195d055ff36e21c9c84118285cb5e780278e
Myeongwon Choi <auspic7@gmail.com>
2020-04-27 23:00:24 +0900

    commented with english

--- Automata.py
+++ Automata.py
@@ -16,55 +16,70 @@ class Automata:
         self.name = None
 
 
+# unused
 class NFA(Automata):
     def __init__(self, alphabet, transition_functions, initial_state, final_states):
         super().__init__(alphabet, transition_functions, initial_state, final_states)
 
 
+# deterministic finite automaton
 class DFA(Automata):
     def __init__(self, alphabet, transition_functions, initial_state, final_states):
         super().__init__(alphabet, transition_functions, initial_state, final_states)
 
+    # gets string and does transition
     def process(self, input_alphabet):
         for char in input_alphabet:
+            # input character not defined: halt
             if char not in self.alphabet:
                 # print(f'{self.name}|{char}: undefined alphabet')
                 self.life = Life.DEAD
                 return self.life
 
+            # automata already halted
             if self.life == Life.DEAD:
                 # print(f'{self.name}|{char}: automata dead')
                 return self.life
 
+            # find transition function with state and input character
             for transition_function in self.transition_functions:
                 if transition_function[0] == self.state and char in transition_function[1]:
+                    # do state transition
                     self.state = transition_function[2]
                     # print(f'{self.name}|{transition_function[0]} ---{char}---> {"("+str(self.state)+")" if self.isfinal() else self.state}')
                     return self.life
+            # transition function not found: halt
             else:
                 # print(f'{self.name}|{char}: function undefined')
                 self.life = Life.DEAD
                 return self.life
 
+    # function that returns if automata is not halted and in final state
     def isfinal(self):
         return self.state in self.final_state and self.life == Life.ALIVE
 
+    # force set automata state and revive
     def setstate(self, state):
         self.state = state
         self.life = Life.ALIVE
 
-
+# utility function generates dfa out of list of lexemes can be accepted
 def generate_dfa_out_of_list(lexemes):
-    alphabet = set(item for sublist in lexemes for item in sublist)  # flatten list and generate alphabet
+    # flatten list and generate alphabet
+    alphabet = set(item for sublist in lexemes for item in sublist)
+    # initialize dfa
     result = DFA(alphabet, list(), 0, list())
 
     state_index = 1
     for word in lexemes:
+        # from starting state to first character state
         result.transition_functions.append((0, word[0], state_index))
 
+        # from first character state to final character state
         for char in word[1:]:
             result.transition_functions.append((state_index, char, state_index + 1))
             state_index += 1
+        # final character state would be the accepting state
         result.final_state.append(state_index)
         state_index += 1
 
--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -10,8 +10,8 @@ parser.add_argument('-l', '--list', help='list shaped output for module use', ac
 args = parser.parse_args()
 
 
+# function for printing error message
 def print_error(text, i):
-    # 에러 메시지 출력
     line_no = text[:i].count('\n') + 1
     line_starting = text[:i].rfind('\n')
     print(f'error occurred on line {line_no}')
@@ -21,76 +21,81 @@ def print_error(text, i):
     return
 
 
-# DFA_LIST를 이용해 string을 lexical analyze
+# lexical analyze string using dfa_list
 def lexically_analyze(string_to_scan, dfa_list):
-    # 결과가 되는 렉심 리스트
+    # lexemes list scanned so far
     lexemes = []
-    # 이번 토큰의 시작지점
+    # starting index of token
     lexeme_start = 0
-    # string을 순회하기 위한 index
+    # index used to traverse string
     i = 0
-    # 후보 dfa, 이번 토큰의 끝지점
+    # candidate dfa, which becomes a dfa used to scan string when every dfa halted
     determined_dfa = lexeme_end = None
 
-    # 파일의 끝까지 순회
+    # traverse file from beginning to end
     while i < len(string_to_scan):
-        # halt되지 않은 dfa의 리스트
+        # list of dfa not halted
         alive_dfa = [item for item in dfa_list if item.process(string_to_scan[i]) == Life.ALIVE]
-        # final state에 있는 dfa의 리스트
+        # list of dfa in final state
         final_dfa = [item for item in dfa_list if item.isfinal()]
 
-        # final state에 있고 살아있는 dfa가 있다면
+        # if there exists a dfa not halted and in final state
         if final_dfa:
-            # 그 중 최상위 우선순위를 가지는 dfa를 후보로 선정, 토큰의 끝 또한 저장
+            # that dfa will be candidate dfa to scan token
             (determined_dfa, lexeme_end) = (final_dfa[0], i)
+        # (else) do nothing because there is some possibility becoming final state when a dfa get new input
 
-        # 모든 dfa가 halt되었다면
+        # when every dfa helts
         if not alive_dfa:
-            # 후보 dfa(lexeme_start부터 찾아봤을 때 가장 긴 토큰을 만드는 dfa)가 존재하지 않는다면(lexeme_start부터 토큰이 없음)
+            # if there is no candidate dfa(a dfa which makes longest token starting from lexeme_start)
+            # which means string from lexeme_start cannot be parsed with dfa provided
             if not determined_dfa:
-                # 에러 메시지 출력
+                # print error message
                 print_error(string_to_scan, i)
                 return
-            # 후보 dfa가 존재하므로 이 dfa가 답이다. lexemes에 렉심과 dfa를 추가함
+            # every dfa halted, and some lexeme was able to parsed. so parse it and add to list
             lexemes.append((string_to_scan[lexeme_start:lexeme_end + 1], determined_dfa.name))
-            # 다음 토큰의 시작지점은 이번 토큰의 끝지점 다음 글자. 당연히 i도 같음
+            # starting point of next token will be ending point of last token + 1
             i = lexeme_start = lexeme_end + 1
-            # i번 글자부터 새로 파싱을 시작하기 위한 초기화
+            # initialization, revive every dfa and remove candidate dfa since we read nothing
             determined_dfa = lexeme_end = None
             for dfa in dfa_list:
                 dfa.setstate(0)
             continue
+        # verbose output
         if args.verbose:
             print(f'alive_dfa({string_to_scan[i]}): {[item.name for item in alive_dfa]}')
             print(f'final_dfa({string_to_scan[i]}): {[item.name for item in final_dfa]}')
             print(f'lexemes({string_to_scan[i]}): {lexemes}')
+        # read next character
         i += 1
 
-    # 마지막 토큰을 parse할 수 없는 경우
+    # could not parse last lexeme
     if not determined_dfa:
+        # print error
         print_error(string_to_scan, i)
         return
-    # 마지막 토큰을 추가
+    # add last lexeme to the list
     lexemes.append((string_to_scan[lexeme_start:lexeme_end + 1], determined_dfa.name))
     return lexemes
 
-# 파일 입력
+
+# file input
 file = open(args.input_file_name)
 file_txt = file.read()
 if args.verbose:
     print(file_txt)
 
-# 토큰 분석
+# lexical analyze
 tokens = lexically_analyze(file_txt, DFAs)
 
-# 에러 없는 경우
+# have no error
 if tokens:
-    # 인자 주어진 경우 그대로 출력
     if args.list:
         print(tokens)
     else:
-        # 표로 만들어서 출력
-        escaped = [(repr(token[0]), token[1]) for token in tokens]
+        # make a fancy table to represent output
+        # escaped = [(repr(token[0]), token[1]) for token in tokens]
         print(tabulate(tokens, ('opt_value', 'token_name'), "fancy_grid"))
 else:
     exit(2)
