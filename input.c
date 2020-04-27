c0f754e3c845f004471c96bddeda5a02e328da6a
Myeongwon Choi <auspic7@gmail.com>
2020-04-27 17:09:08 +0900

    test input c file

---
+++ input.c
@@ -0,0 +1,15 @@
+int func (char c, bool b, float f){
+    int a = 0 + 1200000003 + -1;
+    a = 0-0;
+    char __c1 = "hello world this is an example 123 ";
+    bool b_1234  =  true false;
+    float f_  = 0.000012301 + -1233.3 + 33000000003+ 0.0;
+
+    if(b_1234 != false) {
+        return 12 > 34;
+    } else if {
+        return 1 << 3;
+    }
+}
+
+0 1 22 123 56 001 &$
